17c0ef5ebfb8885d01eda664cf3f3878a33754a4
Paolo Laurenti <laurentipaolo@gmail.com>
2013-11-14 11:04:14 +0100

    Print on STDOUT a sequence of number from 1 to 100

--- app/kata_fizz_buzz.rb
+++ app/kata_fizz_buzz.rb
@@ -1,5 +1,17 @@
 class KataFizzBuzz
 	def printSequence
-		print "1, 2, Fizz, 4, Buzz"
+		sequence = []
+
+		(1...100).each do |number|
+			if number == 3
+				sequence << "Fizz"
+			elsif number == 5
+				sequence << "Buzz"
+			else
+				sequence << number
+			end
+		end
+
+		print sequence.join(", ")
 	end
 end
--- test/kata_fizz_buzz_fixture.rb
+++ test/kata_fizz_buzz_fixture.rb
@@ -3,10 +3,11 @@ require 'stringio'
 require_relative '../app/kata_fizz_buzz'
 
 class KataFizzBuzzFixture < MiniTest::Test
-
+	
 	def setup
 		kataFizzBuzz = KataFizzBuzz.new
 		@output = capture_stdout { kataFizzBuzz.printSequence }
+		@allowedWords = ["Fizz", "Buzz", "FizzBuzz"]
 	end
 
 	def test_should_print_a_sequence_starting_with_1_and_2
@@ -17,9 +18,19 @@ class KataFizzBuzzFixture < MiniTest::Test
 		assert_match /^1, 2, Fizz.*/, @output
 	end
 
-	#Print on STDOUT a sequence of numbers from 1 to 5 with 'Buzz' instead of 5
 	def test_should_print_Buzz_instead_of_5
 		assert_match /^1, 2, Fizz, 4, Buzz.*/, @output
+	end
+
+	def test_should_print_numbers_from_1_to_100
+		items = @output.split(", ")
+		counter = 1
+		items.each do |item|
+			break unless (item.to_i == counter || @allowedWords.include?(item))
+			counter += 1
+		end
+
+		assert_equal 100, counter, "The sequence from 1 to 100 is not respected"
 	end
 
 	private
