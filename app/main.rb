161cb386af13e0105a4b2fe87cef9b09f9069c66
Paolo Laurenti <laurentipaolo@gmail.com>
2013-11-24 10:09:23 +0100

    Main use kata configuation

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,7 @@
 require_relative 'kata_fizz_buzz'
 
-@kata = KataFizzBuzz.new(1, 20)
+fizz_rule_map = RuleMap.new('Fizz', lambda { |number| number.is_divisible_by?(3) || number.has_digit?(3) })
+buzz_rule_map = RuleMap.new('Buzz', lambda { |number| number.is_divisible_by?(5) || number.has_digit?(5) })
+
+@kata = KataFizzBuzz.new(1, 100, [fizz_rule_map, buzz_rule_map])
 @kata.printSequence
