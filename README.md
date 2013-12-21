0a604cc9e9138e0fcba7915366f6b8c911a0c106
Paolo Laurenti <laurentipaolo@gmail.com>
2013-12-21 09:46:31 +0100

    Move Fizz Buzz sources to fizz_buzz

--- README.md
+++
@@ -1,4 +0,0 @@
-ruby_kata_fizz_buzz
-===================
-
-A ruby implementation of the "FizzBuzz" kata (http://codingdojo.org/cgi-bin/wiki.pl?KataFizzBuzz)
--- app/main.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'kata_fizz_buzz'
-
-fizz_rule_map = RuleMap.new('Fizz', lambda { |number| number.is_divisible_by?(3) || number.has_digit?(3) })
-buzz_rule_map = RuleMap.new('Buzz', lambda { |number| number.is_divisible_by?(5) || number.has_digit?(5) })
-
-@kata = KataFizzBuzz.new(1, 100, [fizz_rule_map, buzz_rule_map])
-@kata.printSequence
