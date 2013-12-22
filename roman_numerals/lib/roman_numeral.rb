c190a06638d5a88f8e3e2a497277f503d2aec745
Paolo Laurenti <laurentipaolo@gmail.com>
2013-12-22 10:33:59 +0100

    Code Refatcor

--- roman_numerals/lib/roman_numeral.rb
+++ roman_numerals/lib/roman_numeral.rb
@@ -4,22 +4,33 @@ class RomanNumeral
   end
   
   def get_numbers
-    (1..3).each do |number|
-      @numbers << GetRomanNumeralBetween1And3(number)
-    end
-
-    @numbers << 'IV'
-    @numbers << 'V'
-
-    (6..8).each do |number|
-      i_to_concatenate = GetRomanNumeralBetween1And3(number - 5)
-      @numbers << "V#{i_to_concatenate}"
+    (1..8).each do |number|
+      @numbers << convert(number)
     end
 
     @numbers
   end
 
-  def GetRomanNumeralBetween1And3(number)
+private
+
+  def convert(number)
+    convertedNumber = 0
+    if number <= 3
+      convertedNumber = get_roman_numeral_between_1_and_3(number) 
+    else
+      convertedNumber = 'IV' if number == 4
+      convertedNumber = 'V' if number == 5
+
+      if number >= 6
+        i_to_concatenate = convert(number - 5)
+        convertedNumber = "V#{i_to_concatenate}"
+      end
+    end
+
+    convertedNumber
+  end
+
+  def get_roman_numeral_between_1_and_3(number)
     'I' * number 
   end
 
