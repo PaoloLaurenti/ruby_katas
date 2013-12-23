72582419b90920d62390443a3c2aa3889369b050
Paolo Laurenti <laurentipaolo@gmail.com>
2013-12-23 16:13:05 +0100

    Add conversione of first three numbers

--- roman_numerals/lib/roman_numeral.rb
+++ roman_numerals/lib/roman_numeral.rb
@@ -1,12 +1,25 @@
 class RomanNumeral
   def initialize
-
   end
   
   def get_numbers
-    ['I']
+    roman_numbers = []
+
+    (1..100).each do |number|
+      roman_numbers << convert(number)
+    end
+
+    roman_numbers
   end
 
 private
+
+  def convert(number)
+    if number == 1
+      return 'I'
+    end
+
+    return "I#{convert(number - 1)}"
+  end
 
 end
--- roman_numerals/spec/lib/roman_numeral_spec.rb
+++ roman_numerals/spec/lib/roman_numeral_spec.rb
@@ -8,4 +8,12 @@ describe 'Roman numeral conversion' do
   it 'provides 1 as I' do
     expect(numbers.first).to eq('I')
   end
+
+  it 'provides 2 as II' do
+    expect(numbers[1]).to eq('II')
+  end
+
+  it 'provides 3 as III' do
+    expect(numbers[2]).to eq('III')
+  end
 end  
