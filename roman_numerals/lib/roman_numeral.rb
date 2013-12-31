accea3f44e6be2f64a202744b7833501b5dde64d
Paolo Laurenti <laurentipaolo@gmail.com>
2013-12-31 12:04:53 +0100

    Expose only convert functionality as asked by Kata

--- roman_numerals/lib/roman_numeral.rb
+++ roman_numerals/lib/roman_numeral.rb
@@ -1,22 +1,10 @@
 class RomanNumeral
   def initialize
-    @first_level_symbols = { 1..3 => 'I', 4..4 => 'IV', 5..8 => 'V', 9..9 => 'IX', 
+    @first_level_symbols = {1..3 => 'I', 4..4 => 'IV', 5..8 => 'V', 9..9 => 'IX', 
                             10..39 => 'X', 40..49 => 'XL', 50..89 => 'L', 90..99 => 'XC',
                             100..399 => 'C', 400..499 => 'CD', 500..899 => 'D', 900..999 => 'CM',
                             1000..3000 => 'M'}
   end
-  
-  def get_numbers
-    roman_numbers = []
-
-    (1..3000).each do |number|
-      roman_numbers << convert(number)
-    end
-
-    roman_numbers
-  end
-
-private
 
   def convert(number)
     @first_level_symbols.keys.each do |range|
--- roman_numerals/spec/lib/roman_numeral_spec.rb
+++ roman_numerals/spec/lib/roman_numeral_spec.rb
@@ -6,170 +6,199 @@ describe 'Roman numeral conversion' do
   let (:numbers) { numbers = roman_numeral.get_numbers }
 
   it 'provides 1 as I' do
-    expect(numbers.first).to eq('I')
+    expect(roman_numeral.convert(1)).to eq('I')
   end
 
   it 'provides 2 as II' do
-    expect(numbers[1]).to eq('II')
+    expect(roman_numeral.convert(2)).to eq('II')
   end
 
   it 'provides 3 as III' do
-    expect(numbers[2]).to eq('III')
+    expect(roman_numeral.convert(3)).to eq('III')
   end
 
   it 'provides 4 as IV' do
-    expect(numbers[3]).to eq('IV')
+    expect(roman_numeral.convert(4)).to eq('IV')
   end
 
   it 'provides 5 as V' do
-    expect(numbers[4]).to eq('V')
+    expect(roman_numeral.convert(5)).to eq('V')
   end
 
   it 'provides 6 as VI' do
-    expect(numbers[5]).to eq('VI')
+    expect(roman_numeral.convert(6)).to eq('VI')
   end
 
   it 'provides 7 as VII' do
-    expect(numbers[6]).to eq('VII')
+    expect(roman_numeral.convert(7)).to eq('VII')
   end
 
   it 'provides 8 as VIII' do
-    expect(numbers[7]).to eq('VIII')
+    expect(roman_numeral.convert(8)).to eq('VIII')
   end
 
   it 'provides 9 as IX' do
-    expect(numbers[8]).to eq('IX')
+    expect(roman_numeral.convert(9)).to eq('IX')
   end
 
   it 'provides 10 as X' do
-    expect(numbers[9]).to eq('X')
+    expect(roman_numeral.convert(10)).to eq('X')
   end
 
   it 'provides 11 as XI' do
-    expect(numbers[10]).to eq('XI')
+    expect(roman_numeral.convert(11)).to eq('XI')
   end
 
   it 'provides 12 as XII' do
-    expect(numbers[11]).to eq('XII')
+    expect(roman_numeral.convert(12)).to eq('XII')
   end
 
   it 'provides 13 as XIII' do
-    expect(numbers[12]).to eq('XIII')
+    expect(roman_numeral.convert(13)).to eq('XIII')
   end
 
   it 'provides 14 as XIV' do
-    expect(numbers[13]).to eq('XIV')
+    expect(roman_numeral.convert(14)).to eq('XIV')
   end
 
   it 'provides 15 as XV' do
-    expect(numbers[14]).to eq('XV')
+    expect(roman_numeral.convert(15)).to eq('XV')
   end
 
   it 'provides 16 as XVI' do
-    expect(numbers[15]).to eq('XVI')
+    expect(roman_numeral.convert(16)).to eq('XVI')
   end
 
   it 'provides 18 as XVIII' do
-    expect(numbers[17]).to eq('XVIII')
+    expect(roman_numeral.convert(18)).to eq('XVIII')
   end
 
   it 'provides 19 as XIX' do
-    expect(numbers[18]).to eq('XIX')
+    expect(roman_numeral.convert(19)).to eq('XIX')
   end
+
   it 'provides 20 as XX' do
-    expect(numbers[19]).to eq('XX')
+    expect(roman_numeral.convert(20)).to eq('XX')
   end
+
   it 'provides 39 as XXXIX' do
-    expect(numbers[38]).to eq('XXXIX')
+    expect(roman_numeral.convert(39)).to eq('XXXIX')
   end
+
   it 'provides 40 as XL' do
-    expect(numbers[39]).to eq('XL')
+    expect(roman_numeral.convert(40)).to eq('XL')
   end
+
   it 'provides 41 as XLI' do
-    expect(numbers[40]).to eq('XLI')
+    expect(roman_numeral.convert(41)).to eq('XLI')
   end
+
   it 'provides 49 as XLIX' do
-    expect(numbers[48]).to eq('XLIX')
+    expect(roman_numeral.convert(49)).to eq('XLIX')
   end
+
   it 'provides 50 as L' do
-    expect(numbers[49]).to eq('L')
+    expect(roman_numeral.convert(50)).to eq('L')
   end
+
   it 'provides 53 as LIII' do
-    expect(numbers[52]).to eq('LIII')
+    expect(roman_numeral.convert(53)).to eq('LIII')
   end
+
   it 'provides 78 as LXXVIII' do
-    expect(numbers[77]).to eq('LXXVIII')
+    expect(roman_numeral.convert(78)).to eq('LXXVIII')
   end
+
   it 'provides 89 as LXXXIX' do
-    expect(numbers[88]).to eq('LXXXIX')
+    expect(roman_numeral.convert(89)).to eq('LXXXIX')
   end
+
   it 'provides 90 as XC' do
-    expect(numbers[89]).to eq('XC')
+    expect(roman_numeral.convert(90)).to eq('XC')
   end
+
   it 'provides 93 as XCIII' do
-    expect(numbers[92]).to eq('XCIII')
+    expect(roman_numeral.convert(93)).to eq('XCIII')
   end
+
   it 'provides 98 as XCVIII' do
-    expect(numbers[97]).to eq('XCVIII')
+    expect(roman_numeral.convert(98)).to eq('XCVIII')
   end
+
   it 'provides 99 as XCIX' do
-    expect(numbers[98]).to eq('XCIX')
+    expect(roman_numeral.convert(99)).to eq('XCIX')
   end
+
   it 'provides 100 as C' do
-    expect(numbers[99]).to eq('C')
+    expect(roman_numeral.convert(100)).to eq('C')
   end
 
   it 'provides 103 as CIII' do
-    expect(numbers[102]).to eq('CIII')
+    expect(roman_numeral.convert(103)).to eq('CIII')
   end
+
   it 'provides 149 as CXLIX' do
-    expect(numbers[148]).to eq('CXLIX')
+    expect(roman_numeral.convert(149)).to eq('CXLIX')
   end
+
   it 'provides 199 as CXCIX' do
-    expect(numbers[198]).to eq('CXCIX')
+    expect(roman_numeral.convert(199)).to eq('CXCIX')
   end
+
   it 'provides 399 as CCCXCIX' do
-    expect(numbers[398]).to eq('CCCXCIX')
+    expect(roman_numeral.convert(399)).to eq('CCCXCIX')
   end
+
   it 'provides 400 as CD' do
-    expect(numbers[399]).to eq('CD')
+    expect(roman_numeral.convert(400)).to eq('CD')
   end
+
   it 'provides 499 as CDXCIX' do
-    expect(numbers[498]).to eq('CDXCIX')
+    expect(roman_numeral.convert(499)).to eq('CDXCIX')
   end
+
   it 'provides 500 as D' do
-    expect(numbers[499]).to eq('D')
+    expect(roman_numeral.convert(500)).to eq('D')
   end
+
   it 'provides 503 as DIII' do
-    expect(numbers[502]).to eq('DIII')
+    expect(roman_numeral.convert(503)).to eq('DIII')
   end
+
   it 'provides 845 as DCCCXLV' do
-    expect(numbers[844]).to eq('DCCCXLV')
+    expect(roman_numeral.convert(845)).to eq('DCCCXLV')
   end
+
   it 'provides 901 as CMI' do
-    expect(numbers[900]).to eq('CMI')
+    expect(roman_numeral.convert(901)).to eq('CMI')
   end
+
   it 'provides 999 as CMXCIX' do
-    expect(numbers[998]).to eq('CMXCIX')
+    expect(roman_numeral.convert(999)).to eq('CMXCIX')
   end
+
   it 'provides 1000 as M' do
-    expect(numbers[999]).to eq('M')
+    expect(roman_numeral.convert(1000)).to eq('M')
   end
+
   it 'provides 1003 as MIII' do
-    expect(numbers[1002]).to eq('MIII')
+    expect(roman_numeral.convert(1003)).to eq('MIII')
   end
+
   it 'provides 1999 as MCMXCIX' do
-    expect(numbers[1998]).to eq('MCMXCIX')
+    expect(roman_numeral.convert(1999)).to eq('MCMXCIX')
   end
+
   it 'provides 2000 as MM' do
-    expect(numbers[1999]).to eq('MM')
+    expect(roman_numeral.convert(2000)).to eq('MM')
   end
 
   it 'provides 2999 as MMCMXCIX' do
-    expect(numbers[2998]).to eq('MMCMXCIX')
+    expect(roman_numeral.convert(2999)).to eq('MMCMXCIX')
   end
+
   it 'provides 3000 as MMM' do
-    expect(numbers[2999]).to eq('MMM')
+    expect(roman_numeral.convert(3000)).to eq('MMM')
   end
 
 end  
