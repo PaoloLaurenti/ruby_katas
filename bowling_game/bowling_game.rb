0c0df3da5b1ad1c3feafee5523ddbff37df64a02
Paolo Laurenti <laurentipaolo@gmail.com>
2014-04-26 12:57:46 +0200

    Improve the gutter game test

--- bowling_game/bowling_game.rb
+++ bowling_game/bowling_game.rb
@@ -1,5 +1,8 @@
 class BowlingGame
 
+  def roll(pin)
+  end
+
   def score()
     0
   end
--- bowling_game/spec/bowling_game_spec.rb
+++ bowling_game/spec/bowling_game_spec.rb
@@ -1,9 +1,14 @@
 require_relative '../bowling_game'
 
-describe 'Bowling game' do
+describe "Bowling game" do
   before { @game = BowlingGame.new }
 
-  describe 'when is a gutter game' do
-    it { @game.score.should eq(0) }
+  describe "when is a gutter game" do
+    it "the score is 0" do 
+      (1..10).each do |roll|
+        @game.roll(0)
+      end
+      @game.score.should eq(0) 
+    end
   end
 end
