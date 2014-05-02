c45e8b8d1f86185b00c58172d76f464cd427cd9f
Paolo Laurenti <laurentipaolo@gmail.com>
2014-05-02 15:55:18 +0200

    Berlin clock kata project init

---
+++ berlin_clock_kata/spec/berlin_clock_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe 'Berlin Clock' do
+  it 'xxx' do
+    1.should eql(2)
+  end
+end
--- bowling_game/spec/bowling_game_spec.rb
+++ bowling_game/spec/bowling_game_spec.rb
@@ -1,25 +1,25 @@
 require_relative '../bowling_game'
 
-describe "Bowling game," do
+describe 'Bowling game,' do
   before { @game = BowlingGame.new }
 
-  describe "when is a gutter game," do
-    it "the score should be 0" do
-      rolls_with_same_pins_knocket_out(10, 0)
+  describe 'when is a gutter game,' do
+    it 'the score should be 0' do
+      rolls_with_same_pins_knocked_out(10, 0)
       @game.score.should eq(0)
     end
   end
 
-  describe "when the player has knocket out one pin per frame," do
-    it "the score should be 10" do
-      rolls_with_same_pins_knocket_out(10, 1)
+  describe 'when the player has knocket out one pin per frame,' do
+    it 'the score should be 10' do
+      rolls_with_same_pins_knocked_out(10, 1)
       @game.score.should eq(10)
     end
   end
 
   private
 
-  def rolls_with_same_pins_knocket_out(rolls, pins_per_roll)
+  def rolls_with_same_pins_knocked_out(rolls, pins_per_roll)
     (1..rolls).each do |roll|
         @game.roll(pins_per_roll)
       end
