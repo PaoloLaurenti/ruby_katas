4959daff68b9b4c15b46b48b6e875d1f7e894d28
Paolo Laurenti <laurentipaolo@gmail.com>
2014-05-02 18:52:20 +0200

    Berlin clock should turn off the top yellow lamp when seconds are odds numbers

--- berlin_clock_kata/berlin_clock.rb
+++ berlin_clock_kata/berlin_clock.rb
@@ -1,11 +1,20 @@
 class BerlinClock
 
-  def initialize(instant_timer, listener)
+  def initialize(timer, listener)
     @listener = listener
+    @timer = timer
   end
 
   def start
-    @listener.top_yellow_light_on
+    @timer.add_listener('Berlin Clock', self)
+    @timer.start
   end
 
+  def timer_tick(time)
+    if time.sec.even?
+      @listener.top_yellow_light_on
+    else
+      @listener.top_yellow_light_off
+    end
+  end
 end
--- berlin_clock_kata/spec/berlin_clock_spec.rb
+++ berlin_clock_kata/spec/berlin_clock_spec.rb
@@ -2,19 +2,40 @@ require 'rspec'
 require_relative '../berlin_clock'
 
 describe 'Berlin Clock' do
-  it 'should show top yellow lamp switched on when seconds are even numbers' do
-    instant_timer = InstantTimer.new(DateTime.new(2014, 3, 14, 00, 00, 14))
-    listener = double('listener')
-    listener.should_receive('top_yellow_light_on')
 
-    berlin_clock = BerlinClock.new(instant_timer, listener)
+  before(:each) do
+    @listener = double('listener')
+  end
+
+  it 'should turn on the top yellow lamp when seconds are evens numbers' do
+    instant_timer = InstantTimer.new(Time.gm(2014, 10, 3, 00, 01, 04))
+    @listener.should_receive('top_yellow_light_on')
+
+    berlin_clock = BerlinClock.new(instant_timer, @listener)
+
     berlin_clock.start
+  end
 
+  it 'should turn off the top yellow lamp when seconds are odds numbers' do
+    instant_timer = InstantTimer.new(Time.gm(2014, 10, 3, 00, 01, 13))
+    @listener.should_receive('top_yellow_light_off')
+
+    berlin_clock = BerlinClock.new(instant_timer, @listener)
+
+    berlin_clock.start
   end
 end
 
 class InstantTimer
   def initialize(instant)
     @instant = instant
+  end
+
+  def add_listener(key, listener)
+    @listener = listener
+  end
+
+  def start
+    @listener.timer_tick(@instant)
   end
 end
